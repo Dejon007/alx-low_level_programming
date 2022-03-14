88fe92a62fbb81a756984a26545b05b19bad11e8
Dejon007 <john_oguntimehin@yahoo.com>
2022-03-14 16:33:32 -0700

    my 4th commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
+#incluse <stdio.h>
 /**
  *main - entry point
+ *
  *Return: 0 after printing the function
- */
+ /*
 int main(void)
 {
-puts("/"programming is like building a multilingual puzzle"):
+puts("\"programming is like building a multilingual puzzle"):
 return (0):
+}
