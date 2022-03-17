5ed31be27d90b8448e273c964670bcc323b3b04b
Dejon007 <99606220+Dejon007@users.noreply.github.com>
2022-03-17 17:21:49 +0100

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int i
+
+i = 48;
+
+while (i < 58)
+{
+_putchar (i + '48');
+i++;
+}
+
+_putchar ('\n');
+}
